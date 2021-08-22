3aedb2e6027fb012f7df20d67f0ec715663cfd01
Tiago Martins Ferreira <tiagomf29@hotmail.com>
2021-08-22 01:46:43 -0300

    Ajuste importação swagger

--- contas-api/src/main/java/br/com/contasapi/config/SwaggerConfig.java
+++ contas-api/src/main/java/br/com/contasapi/config/SwaggerConfig.java
@@ -13,10 +13,10 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class SwaggerConfig {
 	@Bean
     public Docket productApi() {
